634f1ff8dbbf71628aaef4b698550d99245679ae
Sannagiri <sannagiriraviteja@gmail.com>
2024-11-06 15:43:10 +0100

    Archives with Testing v2

--- database/migrations/2024_11_06_134711_add_new_columns_for_pdfs_to_external_videos_table.php
+++ database/migrations/2024_11_06_134711_add_new_columns_for_pdfs_to_external_videos_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use App\Models\Resources;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -27,7 +28,7 @@ return new class extends Migration
         }
 
         if (!Resources::where('slug', 'archives')->exists()) {
-            Resources::where('id', 22)->update([
+            DB::table('resources')->where('slug', 'archives')->update([
                 'name' => 'Archives',
                 'slug' => 'archives'
             ]);
